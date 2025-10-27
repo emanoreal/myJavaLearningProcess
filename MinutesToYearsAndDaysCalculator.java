adf728a058bd6abaca9d49da4c1766ce30861591
emanoreal <emanuelgit603@gmail.com>
2025-10-27 10:27:20 -0300

    Add files via upload

---
+++ MinutesToYearsAndDaysCalculator.java
@@ -0,0 +1,20 @@
+public class MinutesToYearsAndDaysCalculator {
+    public static void main(String[] args) {
+        printYearsAndDays(525600);
+        printYearsAndDays(1051200);
+        printYearsAndDays(561600);
+    }
+
+    public static void printYearsAndDays(long minutes) {
+
+        if (minutes < 0 ) {
+            System.out.println("Invalid Value");
+        }
+
+        long days = minutes / 1440;
+        long years = days / 365;
+        long remaingDays = days % 365;
+
+        System.out.println(minutes + " min = " + years + " y and " + remaingDays + " d");
+    }
+}
